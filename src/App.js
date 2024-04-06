72368ed199ab604f318286c7137673e30bd2bd07
bobbyhadz <opmftw@gmail.com>
2024-04-06 15:06:59 +0300

    minor changes

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+import {ReactComponent as MyLogo} from './my-logo.svg';
+
+export default function App() {
+  return (
+    <div>
+      <MyLogo fill="black" stroke="yellow" />
+    </div>
+  );
+}
